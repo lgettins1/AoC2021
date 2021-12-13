dc8e1e2d75e5aaaa2404501e1cb7d445566de25a
Lance Gettins <lance_gettins@hotmail.com>
2021-12-12 22:32:57 -0600

    Day 12 - part 1

--- Day12.java
+++ Day12.java
@@ -16,49 +16,47 @@ public class Day12 {
                 entry ++;
             }
             findend("start", "", lc, 0);
-            System.out.println(pathcount);
+            System.out.println(pathcount + " unique paths.");
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void findend(String f, String path, String[] lc, int lcd){
-        path += f;
-        path += ",";
-
-            for(int a = 0; a < entry; a ++){
-                String nextcave = "";
-                int newlcd = lcd;
-                if(pairs[a][0].trim().equals(f.trim()))nextcave = pairs[a][1];
-                if(pairs[a][1].trim().equals(f.trim()))nextcave = pairs[a][0];
-                switch (nextcave){
-                    case "":
-                    case "start":
-                        break;
-                    case "end":
-                        System.out.println((pathcount + 1) + " - " + path + "end ");
-                        pathcount ++;
-                        break;
-                    default:
-                        int cavegood = 1;
-                        if(nextcave.charAt(0) > 96) {
-                            for (int b = 0; b < newlcd; b++){
-                                if (nextcave.trim().equals(lc[b])) {
-                                   cavegood = 0;
+    public static void findend(String f, String path, String[] littlecave, int lcd){
+        path += f + ",";
+        for(int a = 0; a < entry; a ++){
+            String nextcave = "";
+            int newlcd = lcd;
+            if(pairs[a][0].trim().equals(f.trim()))nextcave = pairs[a][1];
+            if(pairs[a][1].trim().equals(f.trim()))nextcave = pairs[a][0];
+            switch (nextcave){
+                case "":
+                case "start":
+                    break;
+                case "end":
+                    System.out.println((pathcount + 1) + " - " + path + "end ");
+                    pathcount ++;
+                    break;
+                default:
+                    int cavegood = 1;
+                    if(nextcave.charAt(0) > 96) {
+                        for (int b = 0; b < newlcd; b++){
+                            if (nextcave.trim().equals(littlecave[b])) {
+                                cavegood = 0;
                                 break;
-                               }
-                            }
-                            if(cavegood == 1){
-                                lc[newlcd] = nextcave;
-                                newlcd++;
                             }
                         }
-                        if (cavegood == 1) {
-                            findend(nextcave, path, lc, newlcd);
+                        if(cavegood == 1){
+                            littlecave[newlcd] = nextcave;
+                            newlcd++;
                         }
                     }
-                }
+                    if (cavegood == 1) {
+                        findend(nextcave, path, littlecave, newlcd);
+                    }
             }
         }
+    }
+}
 
 
